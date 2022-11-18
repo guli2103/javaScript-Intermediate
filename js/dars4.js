f25f5da598a943fed7fe529a464402962dd6bfab
guli2103 <gulbahorcoder@gmail.com>
2022-11-18 07:59:31 +0500

    javaScript intermediate

---
+++ js/dars4.js
@@ -0,0 +1,61 @@
+alert("Obyetlar")
+
+const odam1 = {
+    ismi: "Jean",
+    familiyasi: " Ichbian",
+    tili: " Ada",
+    yili: "1979-1980-"
+}
+console.log(odam1.ismi + odam1.familiyasi + odam1.tili + " dasturlash tilini " + odam1.yili + "yillarda yaratgan" ) 
+
+const odam2 = {
+    ismi: "Dennis",
+    familiyasi: " Ritchie",
+    tili: " C",
+    yili: "1972-1973-"
+}
+console.log(odam2.ismi + odam2.familiyasi + odam2.tili + " dasturlash tilini " + odam2.yili +"yillarda yaratgan")
+
+const odam3 = {
+    ismi: "Bjarne",
+    familiyasi: " Strostrup",
+    tili: " C++",
+    yili: "1980-"
+}
+console.log(odam3.ismi + odam3.familiyasi + odam3.tili + " dasturlash tilini " + odam3.yili +"yilda yaratgan")
+
+
+const odam4 = {
+    ismi: "Niklaus",
+    familiyasi: " Wirth",
+    tili: " Paskal",
+    yili: "1967-1971-"
+}
+console.log(odam4.ismi + odam4.familiyasi + odam4.tili + " dasturlash tilini " + odam4.yili +"yillarda yaratgan")
+
+
+const odam5 = {
+    ismi: "Djeyms",
+    familiyasi: " Gosling",
+    tili: " Java",
+    yili: "1995-"
+}
+console.log(odam5.ismi + odam5.familiyasi + odam5.tili + " dasturlash tilini " + odam5.yili +"yillarda yaratgan")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
