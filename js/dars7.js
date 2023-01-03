130640f8933f2ab0fd5ae3e21c7358910f8e541c
guli2103 <gulbahorcoder@gmail.com>
2023-01-03 19:51:54 +0500

    dars7

---
+++ js/dars7.js
@@ -0,0 +1,13 @@
+function Mashina(nomi,rangi){
+    this.nomi = nomi
+    this.rangi = rangi
+}
+const mashina1 = new Mashina('BMW', 'oq')
+const mashina2 = new Mashina('LAbo', 'qora')
+const mashina3 = new Mashina('Ferrari', 'qizil')
+const mashina4 = new Mashina('Chevrolet', "ko'k")
+
+console.log(mashina1)
+console.log(mashina2.nomi)
+console.log(mashina3.rangi)
+console.log(mashina4.nomi)
